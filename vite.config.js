57b24b648d0e4e5a923975393558b9c4d8b0182f
Franh01 <fran_lopez9@live.com>
2023-11-21 17:56:51 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "jsx",
-      enforce: "pre",
-      transform(code, id) {
-        if (id.endsWith(".js") && code.includes("jsx")) {
-          return {
-            code: code.replace(
-              /(import.*from.*['"])(.*\.(jsx|js))(['"])/g,
-              "$1$2$3"
-            ),
-            map: null,
-          };
-        }
-      },
-    },
-  ],
-  esbuild: {
-    jsxFactory: "React.createElement",
-    jsxFragment: "React.Fragment",
-  },
+  plugins: [react()],
   server: {
     host: true,
     port: 3000,
